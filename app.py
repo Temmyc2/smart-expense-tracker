c3fc123abb0d20efcc6695e2c0999267b132e541
Temmyc2 <temmyolufowose@gmail.com>
2026-05-31 02:01:28 +0100

    Fix secret key and remove duplicate README

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from dotenv import load_dotenv
 
 from helpers import login_required, format_currency, greet
 
 # Configure application
 app = Flask(__name__)
-app.secret_key = "dev_key"
+load_dotenv()
+app.secret_key = os.environ.get("SECRET_KEY")
 
 # Custom filter
 app.jinja_env.filters['format_currency'] = format_currency
